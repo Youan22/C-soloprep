51d5f2acde0fd5d322242b435a1444c2db23f86d
Youan22 <you18034@byui.edu>
2022-04-23 18:50:33 -0600

    Solo_prop_update

--- solo_prep_1/Program.cs
+++ solo_prep_1/Program.cs
@@ -6,8 +6,17 @@ namespace solo_prep_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is solo prep 1");
+           // Console.WriteLine("This is solo prep 1");
+            Console.WriteLine();
+            Console.Write("What is your first name? ");
+            String fName = Console.ReadLine();
 
+            Console.Write("What is your last name? ");
+            String lName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Your name is {fName},{fName} {lName}.");
+            Console.WriteLine();
             // Write your code here
         }
     }
