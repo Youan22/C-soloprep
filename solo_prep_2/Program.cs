839747b31deef9ad347c027de949ac4bf5a7d5bd
Youan22 <you18034@byui.edu>
2022-04-25 00:03:51 -0600

    Soloprep2

--- solo_prep_2/Program.cs
+++ solo_prep_2/Program.cs
@@ -6,7 +6,41 @@ namespace solo_prep_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your grade: "); 
+            String gradeFromUser = Console.ReadLine();
+
+            int grade = int.Parse(gradeFromUser);
+           
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Your letter grade is: A");
+            }
+            else if (grade >= 80) 
+            {
+                Console.WriteLine("Your letter grade is: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Your letter grade is: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Your letter grade is: D");
+            }
+            else
+            {
+                Console.WriteLine("Your letter grade is: F");
+            }
+            if (grade >= 70)
+            {
+                Console.WriteLine("Congratulation! You passed the class.");
+            }
+            else
+            {
+                Console.WriteLine("Please, take your studies more serious in the future!");
+            }
+
         }
     }
-}
+}        
