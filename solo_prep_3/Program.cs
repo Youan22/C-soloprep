4b82e38a657f476e1be24a8aa1677f8d2d80f14c
Youan22 <you18034@byui.edu>
2022-04-25 12:17:59 -0600

    Solo prep 3

--- solo_prep_3/Program.cs
+++ solo_prep_3/Program.cs
@@ -6,7 +6,33 @@ namespace solo_prep_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random randomGenerator = new Random();
+           
+            int magicNumber = randomGenerator.Next(1,101);
+
+            int guess = -1;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess ?" );
+
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                Console.WriteLine("Higher");
+        
+                }
+                else if (magicNumber < guess)
+                {
+                Console.WriteLine("Lower");
+                }
+                else
+                {
+                Console.WriteLine("Yay!!! You guessed it!");
+                }
+
+            }
         }
     }
 }
